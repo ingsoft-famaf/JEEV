a5d2a21f531fd6c8375cf839c189b895f32d329e
virgig <virginiagiglio58@gmail.com>
2016-10-19 14:07:36 -0300

    nuevo test de signup

--- mysite/users/tests.py
+++ mysite/users/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from allauth.account.forms import BaseSignupForm
 from django.test.client import Client
+from allauth.utils import get_user_model
 
 
 class SignupTest(TestCase):
@@ -19,4 +20,22 @@ class SignupTest(TestCase):
 			'form',
 			'password2',
 			'You must type the same password each time.'
+		)
+
+	def test_signup_username_already_exist(self):
+		user = get_user_model().objects.create(
+			username='pedro',
+			is_active=True)
+		response = self.client.post(
+				reverse('account_signup'),
+				data={
+					'username': 'pedro',
+					'email': 'pedro@test.com',
+					'password1': 'pepito12',
+					'password2': 'pepito12'})
+		self.assertFormError(
+			response,
+			'form',
+			'username',
+			'A user with that username already exists.'
 		)
