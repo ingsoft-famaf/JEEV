73e04ab940b8e3c445489670c4dccf3c4508bd7a
virgig <virginiagiglio58@gmail.com>
2016-10-19 22:22:50 -0300

    modificaciones en setting para redirigir logout a login, se borró class q no se usan en views.py, arreglos de pep8 en tests y urls

--- mysite/users/tests.py
+++ mysite/users/tests.py
@@ -3,54 +3,64 @@ from django.core.urlresolvers import reverse
 from allauth.account.forms import BaseSignupForm
 from django.test.client import Client
 from allauth.utils import get_user_model
+from django.contrib.auth.models import User
 
 
 class SignupTest(TestCase):
 
-	def test_signup_password_twice_form_error(self):
-		response = self.client.post(
-				reverse('account_signup'),
-				data={
-					'username': 'pedro',
-					'email': 'pedro@test.com',
-					'password1': 'pepito12',
-					'password2': 'pepoti12'})
-		self.assertContains(response, 'You must type the same password each time.')
+    def test_signup_password_twice_form_error(self):
+        response = self.client.post(
+                reverse('account_signup'),
+                data={
+                    'username': 'pedro',
+                    'email': 'pedro@test.com',
+                    'password1': 'pepito12',
+                    'password2': 'pepoti12'})
+        self.assertContains(response, 'You must type the same password each time.')
 	
-	def test_signup_password_too_short(self):
-		response = self.client.post(
-				reverse('account_signup'),
-				data={
-					'useranme': 'pedro',
-					'email': 'pedro@test.com',
-					'password1': 'pepi',
-					'password2': 'pepi'})
-		self.assertContains(response, 'This password is too short. It must contain at least 8 characters.')
+    def test_signup_password_too_short(self):
+        response = self.client.post(
+                reverse('account_signup'),
+                data={
+                    'useranme': 'pedro',
+                    'email': 'pedro@test.com',
+                    'password1': 'pepi',
+                    'password2': 'pepi'})
+        self.assertContains(response, 'This password is too short. It must contain at least 8 characters.')
 
-	def test_signup_username_already_exist(self):
-		user = get_user_model().objects.create(
-			username='pedro',
-			is_active=True)
-		response = self.client.post(
-				reverse('account_signup'),
-				data={
-					'username': 'pedro',
-					'email': 'pedro@test.com',
-					'password1': 'pepito12',
-					'password2': 'pepito12'})
-		self.assertContains(response, 'A user with that username already exists.')
+    def test_signup_password_similar_username(self):
+        response = self.client.post(
+                reverse('account_signup'),
+                data={
+                    'username': 'pepito',
+                    'email': 'pepito@test.com',
+                    'password1': 'pepito12',
+                    'password2': 'pepito12'})
+        self.assertContains(response, 'The password is too similar to the username.')
 
+    def test_signup_username_already_exist(self):
+        user = get_user_model().objects.create(
+            username='pedro',
+            is_active=True)
+        response = self.client.post(
+                reverse('account_signup'),
+                data={
+                    'username': 'pedro',
+                    'email': 'pedro@test.com',
+                    'password1': 'pepito12',
+                    'password2': 'pepito12'})
+        self.assertContains(response, 'A user with that username already exists.')
 
-	def test_signup_email_already_exist(self):
-		user = get_user_model().objects.create(
-			username='juan',
-			email='pedro@test.com',
-			is_active=True)
-		response = self.client.post(
-				reverse('account_signup'),
-				data={
-					'username': 'pedro',
-					'email': 'pedro@test.com',
-					'password1': 'pepito12',
-					'password2': 'pepito12'})
-		self.assertContains(response,'A user is already registered with this e-mail address.')	
+    def test_signup_email_already_exist(self):
+        user = get_user_model().objects.create(
+            username='juan',
+            email='pedro@test.com',
+            is_active=True)
+        response = self.client.post(
+            reverse('account_signup'),
+            data={
+                    'username': 'pedro',
+                    'email': 'pedro@test.com',
+                    'password1': 'pepito12',
+                    'password2': 'pepito12'})
+        self.assertContains(response,'A user is already registered with this e-mail address.')	
--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.conf.urls import url
-from allauth.account.views import SignupView
+from allauth.account.views import SignupView, LogoutView
 
 urlpatterns = [
     url(r'^login/', views.login_view, name='login'),
-    url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^logout/', LogoutView, name='account_logout'),
     url(r'^signup/', SignupView, name='account_signup'),
     url(r'^profile/', views.profile_view, name='profile'),
 ]
