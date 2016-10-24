aa6a312f10ef8272125d745c11025de59b36038f
EliQuiroga <sherutiny@gmail.com>
2016-10-24 20:51:23 -0300

    Ya se puede ir a la pagina de login del proyecto al hacer logout desde la plataforma de admin

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -7,11 +7,11 @@ from django.contrib.auth.models import (
 )
 
 
-class MyAdmin(AbstractBaseUser):
+#class MyAdmin(AbstractBaseUser):
 
-    is_admin = models.BooleanField(default=False)
+#    is_admin = models.BooleanField(default=False)
 
-    def is_staff(self):
-        "Is the user a member of staff?"
+#    def is_staff(self):
+#        "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
-        return self.is_admin
+#        return self.is_admin
