011ac44faa6ee00f5f55061a3809e6170ffdd5ff
Esequiel <Esequiel1308@hotmail.com>
2016-10-24 23:19:04 -0300

    subidos parte de crear pregunta, falta respuesta

--- mysite/questions/models.py
+++ mysite/questions/models.py
@@ -6,32 +6,6 @@ from django.db import models
 
 
 class Question(models.Model):
-	NombreTema = models.CharField(max_length=50)
-	NombreSubtema = models.CharField(max_length=50)
-	NombreMateria = models.CharField(max_length=50)
-	NumeroPreg = models.IntegerField()
-	ValorPreg = models.IntegerField()
+	NombreTema = models.CharField(max_length=100)
+	NombreMateria = models.CharField(max_length=100)
 	TextPreg = models.CharField(max_length=200)
-
-"""
-	E = ElementMaker()
-	ROOT = E.root
-	DOC = E.doc
-	TEMA = E.field1
-	SUBTEMA = E.field6
-	FIELD2 = E.field2
-	MATERIA = E.field3
-	VALOR = E.field4
-	NUMERO = E.field5
-
-	the_doc = ROOT(
-    				DOC(
-        				TEMA("%s" % (NombreTema), name = 'tema'),
-        				SUBTEMA("%s" % (NombreSubtema), name = 'subtema'),
-
-    				)
-
-				)
-
-
-#E.TD("%s %s" % (row_num, col_num)) for col_num in range(3)R"""
--- mysite/questions/views.py
+++ mysite/questions/views.py
@@ -8,28 +8,65 @@ from models import Question
 #@login_required
 #@permission_required('is_superuser')
 
+
 class question_view():
+    
+
+    def get_distancia(str1,str2):
+        d=dict()
+        for i in range(len(str1)+1):
+            d[i]=dict()
+            d[i][0]=i
+        for i in range(len(str2)+1):
+            d[0][i] = i
+        for i in range(1, len(str1)+1):
+            for j in range(1, len(str2)+1):
+                d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
+        return d[len(str1)][len(str2)]
+
+
     URL_TO_PARSE = '/home/usuario/Escritorio/XML/preg.xml'
-    # URL_TO_PARSE=
-    # '/home/usuario/Escritorio/Ingenieria/JEEV/static/Exams/preg.xml'
     URL = StringIO(URL_TO_PARSE)
     tree = etree.parse(URL_TO_PARSE)
     root = tree.getroot()
-    child = root.getchildren()
-    print(child)
-    print ('feo')
-    """E = ElementMaker()
-    ROOT = E.root
-    DOC = E.doc
-    TEMA = E.field1
-    SUBTEMA = E.field2
-    MATERIA = E.field3
-    VALOR = E.field4
-    NUMERO = E.field5 
-    the_doc = ROOT(
-    				DOC(
-    					TEMA("%s"(NombreTema), nombre='tema'),
-    					SUBTEMA("%s"(NombreSubtema), nombre='subtema'),
-    					MATERIA("%s"(NombreMateria), nombre ='materia'),
-    				)
-			)"""
+    for pregunta in root:
+        materia = pregunta.find('materia').text
+        tema = pregunta.find('tema').text
+        texto = pregunta.find('texto').text
+        # print type(tema)
+        print materia
+        print tema
+        # q = Question(NombreTema=tema,NombreMateria = materia,TextPreg=texto)
+        # q.save()
+        query = Question.objects.filter(
+            NombreTema=tema).filter(NombreMateria=materia)
+
+        count = query.count()
+        print ('el conteo de preguntas con mismo tema y materia: %s' % count)
+        if count == 0:
+            #q = Question(NombreTema=tema,NombreMateria = materia,TextPreg=texto)
+            #q.save()
+            print('Pregunta creada')
+        else:
+             for i in range(count):
+                print ('la i en el bucle es %s' % i)
+                firstObj = query[i]
+                print firstObj.TextPreg
+                print texto
+                print ('la distancia es %s' % get_distancia(firstObj.TextPreg,texto))
+                if get_distancia(firstObj.TextPreg,texto) == 0:
+                    print('pregunta repetida')
+                else:
+                    #q = Question(NombreTema=tema,NombreMateria = materia,TextPreg=texto)
+                    #q.save()
+                    print('pregunta creada 2')
+    #all_objects = Question.objects.all().delete()
+    # qobject = Question.objects.get(NombreTema= tema)
+    # print qobject.NombreMateria
+    # print(root[1][0].text)
+    """
+            for i in range(count):
+                firstObj = query[i]
+                print firstObj.TextPreg
+    """
+    print('feo')
