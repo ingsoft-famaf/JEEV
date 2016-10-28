960a9d882940d5c8afbf00d9b4b182f5f6e924e5
EliQuiroga <sherutiny@gmail.com>
2016-10-28 15:59:47 -0300

    se remueven la carpeta question

--- questions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Question
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fields = ['NombreTema', 'NombreMateria', 'TextPreg']
-
-admin.site.register(Question, QuestionAdmin)
--- questions/urls.py~
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-    url(r'^question/', views.question_view, name = 'question'),
-]
