70756dee2fa6f09180940eae6e279744d7f249c9
julian :D <julian-allende@hotmail.com>
2016-10-31 20:51:49 -0300

    reparacion de redireccion de examenperfil

--- mysite/examenes/views.py
+++ mysite/examenes/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def examen_view(request):
-    return render(request, 'examenes/examenPerfil.html', {})
-
+def examenperfil(request):
+    print ("hola")
+    return render(request, 'examenes/examenperfil.html', {})
