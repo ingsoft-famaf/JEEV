7667d8245dad5d87f546f58ee6b814a4095f88aa
EliQuiroga <sherutiny@gmail.com>
2016-11-01 00:28:13 -0300

    se crean las view cargarmateria y cargartema

--- mysite/materias/views.py
+++ mysite/materias/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def cargarmateria(request):
+    return render(request, 'materias/cargarmateria.html', {})
+
+def cargartema(request):
+    return render(request, 'materias/cargartema.html', {})
