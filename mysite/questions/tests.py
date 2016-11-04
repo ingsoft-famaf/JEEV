0f5955f32b5ab633d8457161862219ffda118d49
Esequiel <Esequiel1308@hotmail.com>
2016-11-04 17:26:28 -0300

    asd

--- mysite/questions/tests.py
+++ mysite/questions/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from questions.models import Question
 
 # Create your tests here.
+
+
+class UploadQuestionTest(TestCase):
+
+	def __init__(self):
+		quiestion_view('/home/usuario/Escritorio/XML/preg.xml')		
+
+	def test_verify_upload(self):
+		question = Question.object.get(materia="Alegra")
+		materia = question.find('materia').text
+        #tema = question.find('tema').text
+        #texto = question.find('texto').text
+        self.assertEqual(materia, 'Algebra')
+        #self.assertEqual(tema, 'matrices')
+        #self.assertEqual(text, 'Esta matriz es inversible?')
