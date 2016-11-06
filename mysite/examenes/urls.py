351f9e4aded89947f0b8a2dd655231f017ecb007
virgig <virginiagiglio58@gmail.com>
2016-11-06 19:57:12 -0300

    Se implementó tiempo en examen y posibilidad de reportar una pregunta

--- mysite/examenes/urls.py
+++ mysite/examenes/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^(?P<examen_id>[0-9]+)/(?P<pregunta_id>[0-9]+)/reportar', views.reportar, name='reportar'),
     url(r'^(?P<examen_id>[0-9]+)/respuesta', views.respuesta, name='respuesta'),
     url(r'^(?P<examen_id>[0-9]+)/resppreg', views.resppreg, name='resppreg'),
     url(r'^examenencurso', views.examenencurso_view, name='examenencurso'),
--- mysite/examenes/views.py
+++ mysite/examenes/views.py
@@ -26,7 +26,7 @@ def examenencurso_view(request):
 #    print materia
 #    print tema
 #    print cantidad
-#    print tiempo
+    print tiempo
     #preguntas_respondidas = []
     examen = Exam(nombre_materia = materia,nombre_tema = tema,
                     cantidad_preg = cantidad, tiempo_preg = tiempo)
@@ -52,29 +52,39 @@ def resppreg(request, examen_id):
     examen = get_object_or_404(Exam, pk=examen_id)
     tema = examen.nombre_tema
     materia = examen.nombre_materia
-    tiempo = 30
+    cantidad = examen.cantidad_preg
+    cantidad = int(cantidad)
     randomm =[]
-    query = Question.objects.filter(
-        nombre_tema=tema).filter(
-            nombre_materia=materia)
-    randomm = random.sample(query, 1)
+    query1 = Question.objects.filter(nombre_tema=tema)
+    query2 = query1.filter(nombre_materia=materia)
+    query3 = query2.filter(reportada=False)
+    randomm = random.sample(query3, 1)
     #materia = Exam.objects.filter(id=)
     pregunta = randomm[0]
 #    print examen.pregunta_actual
 #    print examen.cantidad_preg
     if examen.pregunta_actual == examen.cantidad_preg:
-        print "hola"
+        return render(request, 'examenes/finalizo.html')
     return render(request,'examenes/resppreg.html',
-                    {'pregunta': pregunta,'examen':examen})
+                  {'pregunta': pregunta,'examen':examen})
 
 def respuesta(request, examen_id):
+
     if request.method == 'POST':
         respuesta_id = request.POST['respuesta']
         examen = get_object_or_404(Exam, pk=examen_id)
         examen.pregunta_actual +=1
         examen.save()
         respuesta = get_object_or_404(Answer, pk=respuesta_id)
-        return render(request, 'examenes/respuesta.html',{'respuesta':respuesta, 'examen':examen})
+        return render(request, 'examenes/respuesta.html',
+                      {'respuesta':respuesta, 'examen':examen})
+    examen = get_object_or_404(Exam, pk=examen_id)
+    return render(request, 'examenes/respuesta.html', {'examen':examen})
 
-#    print respuesta
-    return render(request, 'examenes/respuesta.html')
+def reportar(request, examen_id, pregunta_id):
+    examen = get_object_or_404(Exam, pk=examen_id)
+    pregunta = get_object_or_404(Question, pk=pregunta_id)
+    pregunta.reportada = True
+    pregunta.save()
+    return render(request, 'examenes/respuesta.html',
+                  {'pregunta': pregunta, 'examen': examen})
