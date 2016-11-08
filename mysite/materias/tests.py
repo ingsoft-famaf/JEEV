fb599b8048e3342d76ec3b67437f920e08a9a4fc
virgig <virginiagiglio58@gmail.com>
2016-11-08 12:33:03 -0300

    test de materia

--- mysite/materias/tests.py
+++ mysite/materias/tests.py
@@ -2,17 +2,22 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.test.client import Client
-
+from materias.views import *
 from .models import Materia, Tema
 
 class MateriaMethodTests(TestCase):
+
+    def setUp(self):
+        materia1 = Materia.objects.create(nombre_materia='Biologia')
+
+        materia2 = Materia.objects.create(nombre_materia='Algebra')
 
     def test_se_cargo_materia_correctamente(self):
         response = self.client.post(
                 reverse('cargarm'),
                 data={
-                    'nueva_materia': 'biologia'})
-        self.assertRedirects(response, '/materias/secargo/', fetch_redirect_response=False)
+                    'nueva_materia': 'Quimica'})
+        self.assertEqual(response.resolver_match.func, cargarm)
 
 class TemaMethodTests(TestCase):
 
