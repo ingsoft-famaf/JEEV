e83cdde0b21dc7f20afa8071d5c2c2b7eedc3b6e
virgig <virginiagiglio58@gmail.com>
2016-11-12 20:16:07 -0300

    se arregla grafico de estadisticas y se crean los botones para volver

--- mysite/estadisticas/views.py
+++ mysite/estadisticas/views.py
@@ -4,8 +4,11 @@ from examenes.models import Exam
 from materias.models import Materia
 from django.db.models import Avg, Sum, FloatField
 import scipy as sp
-from matplotlib import pyplot
+from matplotlib import pyplot, pylab
+from pylab import *
 import matplotlib.pyplot as plt
+import cStringIO
+from cStringIO import StringIO
 
 
 def grafico(request, materia):
@@ -14,7 +17,7 @@ def grafico(request, materia):
     # Creamos el array y conde cada punto es el seno de cada elemento de x
     #y = sp.sin(x)
     # Creamos una figura
-    #plt.figure()
+    #fig = plt.figure()
     lista_examenes = Exam.objects.filter(nombre_materia=materia)
     porcentajes = []
     for x in xrange(lista_examenes.count()):
@@ -26,7 +29,6 @@ def grafico(request, materia):
     listx = []
     for x in xrange(1,lista_examenes.count()+1):
         listx.append(x)
-    print listx
     plt.plot(listx,y)
     #plt.title('Grafico de %s', )
     plt.xlabel('tiempo en dias')
@@ -34,38 +36,35 @@ def grafico(request, materia):
 
     # Mostramos en pantalla
     plt.show()
+    #graphic = cStringIO.StringIO()
+
+    return render(request, 'estadisticas/graphic.html', {'materia':materia})
+
+
+def estadis(request, materia):
+    lista_examenes = Exam.objects.filter(nombre_materia=materia)
+    cant_preguntas = lista_examenes.aggregate(Sum('cantidad_preg'))
+    preg_correctas = lista_examenes.aggregate(Sum('preguntas_correctas'))
+    preg_incorrectas = lista_examenes.aggregate(Sum('preguntas_incorrectas'))
+    promedio = lista_examenes.aggregate(Avg('preguntas_correctas'))
+    promedio= (promedio['preguntas_correctas__avg'])*10
+    return render(request, 'estadisticas/general.html',
+                  {'lista_examenes': lista_examenes, 'cant_preguntas': cant_preguntas,
+                   'preg_correctas':preg_correctas, 'preg_incorrectas': preg_incorrectas,
+                   'materia': materia, 'promedio': promedio})
 
 
 def estadistica_view(request):
-    if request.method == "POST":
-        materia = request.POST['materia']
-        lista_examenes = Exam.objects.filter(nombre_materia=materia)
-        cant_preguntas = lista_examenes.aggregate(Sum('cantidad_preg'))
-        preg_correctas = lista_examenes.aggregate(Sum('preguntas_correctas'))
-        preg_incorrectas = lista_examenes.aggregate(Sum('preguntas_incorrectas'))
-        promedio = lista_examenes.aggregate(Avg('preguntas_correctas'))
-        promedio= (promedio['preguntas_correctas__avg'])*10
-        return render(request, 'estadisticas/general.html',
-                      {'lista_examenes': lista_examenes, 'cant_preguntas': cant_preguntas,
-                       'preg_correctas':preg_correctas, 'preg_incorrectas': preg_incorrectas,
-                       'materia': materia, 'promedio': promedio})
     lista_materias = Exam.objects.values_list(
-                            'nombre_materia', flat=True).distinct()
-
+                        'nombre_materia', flat=True).distinct()
     for x in xrange(lista_materias.count()):
-        """
-        lista_examenes = Exam.objects.filter(nombre_materia=lista_materias[x])
-        promedios.append(lista_examenes.aggregate(Avg('preguntas_correctas')))
-        print promedios[x]
-        """
         lista_examenes = Exam.objects.filter(nombre_materia=lista_materias[x])
         promedio = lista_examenes.aggregate(Avg('preguntas_correctas'))
         materia = get_object_or_404(Materia, nombre_materia=lista_materias[x])
         materia.promedio = (promedio['preguntas_correctas__avg'])*10
         materia.save()
     materias = Materia.objects.all()
-    return render(request, 'estadisticas/estadis.html',
-                  {'materias': materias})
+    return render(request, 'estadisticas/estadis.html', {'materias': materias})
 
 
 def estadis_examen(request, examen_id):
