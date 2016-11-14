143cec25a6f9f81fbf4e6e4a300c0c03a23b8fca
EliQuiroga <sherutiny@gmail.com>
2016-11-13 23:41:55 -0300

    Cargar materias y cargar temas son  case sensitive

--- mysite/materias/views.py
+++ mysite/materias/views.py
@@ -4,36 +4,27 @@ from django.shortcuts import render
 from models import Materia, Tema
 from Levenshtein import *
 
-
-# Las tres funciones de esta view toman HttpRequest como su primer parámetro y
-# devuelven una instancia de HttpResponse utilizando el atajo render.
-# Los parámetros de render son request y template_name.
-
-
-# cargarm 
 def cargarm(request):
     if request.method == "POST":
         nuevamat = request.POST['nueva_materia']
-	if nuevamat == "" or nuevamat == " ":
-		return render(request, 'materias/repetida.html')
-        if type(nuevamat) != unicode:
+        if nuevamat == "" or nuevamat == " ":
             return render(request, 'materias/repetida.html')
-        query = Materia.objects.filter(nombre_materia=nuevamat)
-        count = query.count()       
+        query = Materia.objects.filter(nombre_materia__iexact=nuevamat)
+        repetida = False
+        count = query.count()
+        q = Materia(nombre_materia=nuevamat)
         if count == 0:
-            nmat = Materia(nombre_materia=nuevamat)
-            nmat.save()
+            q.save()
+            return render(request, 'materias/secargo.html')
         else:
-            repetida = False
-            nmat = Materia(nombre_materia=nuevamat)
-            if distance(str(nmat), str(nuevamat)) <= 0:
+            if distance(str(query[0]).lower(), str(nuevamat).lower()) == 0:
                 repetida = True
             if repetida is False:
-                nmat = Materia(nombre_materia=nuevamat)
-                nmat.save()
+                q = Materia(nombre_materia=nuevamat)
+                q.save()
+                return render(request, 'materias/secargo.html')
             else:
                 return render(request, 'materias/repetida.html')
-    	return render(request, 'materias/secargo.html')
     return render(request, 'materias/cargarmateria.html')
 
 
@@ -41,27 +32,25 @@ def cargart(request):
     if request.method == "POST":
         materia = request.POST['materias']
         nuevotema = request.POST['nuevo_tema']
-	if nuevotema == "" or nuevotema == " ":
-		return render(request, 'materias/temarepetido.html')
-	else:
-        	mat = Materia.objects.filter(nombre_materia = materia)
-        print mat
-        query = Tema.objects.filter(nombre_tema=nuevotema)
-        count = query.count()       
+        if nuevotema == "" or nuevotema == " ":
+            return render(request, 'materias/temarepetido.html')
+        mat = Materia.objects.filter(nombre_materia = materia)
+        query = Tema.objects.filter(nombre_tema__iexact=nuevotema)
+        repetida = False
+        count = query.count()
+        ntema = Tema(temas= mat[0], nombre_tema=nuevotema)
         if count == 0:
-            ntema = Tema(temas= mat[0], nombre_tema=nuevotema)
             ntema.save()
+            return render(request, 'materias/secargo.html')
         else:
-            repetida = False
-            ntema = Tema(temas= mat[0], nombre_tema=nuevotema)
-            if distance(str(ntema), str(nuevotema)) <= 0:
+            if distance(str(query[0]).lower(), str(nuevotema).lower()) == 0:
                 repetida = True
             if repetida is False:
                 ntema = Tema(temas= mat[0], nombre_tema=nuevotema)
                 ntema.save()
+                return render(request, 'materias/secargo.html')
             else:
-        	   return render(request, 'materias/temarepetido.html')
-        return render(request, 'materias/secargo.html')
+               return render(request, 'materias/temarepetido.html')
     return render(request, 'materias/cargartema.html',{'list_materias': Materia.objects.values_list(
                         'nombre_materia', flat=True).distinct()})
 
@@ -71,4 +60,3 @@ def obtener_tema_materia(request):
     return render(request, 'materias/cargartema.html',
               {'list_materias': Materia.objects.values_list(
                         'nombre_materia', flat=True).distinct()})
-
