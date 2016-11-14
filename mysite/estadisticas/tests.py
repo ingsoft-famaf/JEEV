b06f7aa5fafce6920cc38e8490ed47abf88425bd
virgig <virginiagiglio58@gmail.com>
2016-11-14 20:39:17 -0300

    se agrega un test de estadistica, se comentan los print en los test de materias

--- mysite/estadisticas/tests.py
+++ mysite/estadisticas/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from examenes.models import Exam
+from materias.models import Materia
+from estadisticas.views import *
 
-# Create your tests here.
+class EstadisticasTest(TestCase):
+
+    def setUp(self):
+        materia1 = Materia.objects.create(nombre_materia='Algebra')
+        materia2 = Materia.objects.create(nombre_materia='Lengua')
+        materia3 = Materia.objects.create(nombre_materia='Biologia')
+        examen1 = Exam.objects.create(nombre_materia = 'Lengua',
+                    nombre_tema = 'Lectura', cantidad_preg = 3,
+                    tiempo_preg = 14, preguntas_correctas = 2,
+                    preguntas_incorrectas = 1)
+        examen4 = Exam.objects.create(nombre_materia = 'Lengua',
+                    nombre_tema = 'Escritura', cantidad_preg = 6,
+                    tiempo_preg = 14, preguntas_correctas = 4,
+                    preguntas_incorrectas = 2)
+        examen2 = Exam.objects.create(nombre_materia = 'Algebra',
+                    nombre_tema = 'Matrices', cantidad_preg = 5,
+                    tiempo_preg = 10, preguntas_correctas = 2,
+                    preguntas_incorrectas = 3)
+        examen3 = Exam.objects.create(nombre_materia = 'Algebra',
+                    nombre_tema = 'Combinatoria', cantidad_preg = 4,
+                    tiempo_preg = 9, preguntas_correctas = 2,
+                    preguntas_incorrectas = 2)
+
+    def test_lista_materias(self):
+        response = self.client.get(reverse('estadisticas'))
+        #print response
+        self.assertEqual(response.resolver_match.func, estadistica_view)
+
+    def test_estadistica_materia(self):
+        materia = Materia.objects.get(nombre_materia='Lengua')
+        response = self.client.get(reverse('estadis', args=[materia]))
+        #print response
+        self.assertEqual(response.resolver_match.func, estadis)
+
+    def test_(self):
+        pass
