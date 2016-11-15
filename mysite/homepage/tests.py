0cdf2ec136a6bcd0e550eb3889430e358c9ffffb
Mazsy <maxite71@gmail.com>
2016-11-15 15:53:02 -0300

    2 test de homepage

--- mysite/homepage/tests.py
+++ mysite/homepage/tests.py
@@ -1,3 +1,17 @@
+#-*- coding: utf-8 -*-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 
-# Create your tests here.
+from homepage.views import *
+
+class HomeTests(TestCase):
+
+    def test_redirijido_al_home(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEquals(response.resolver_match.func,  homepage)
+
+class TemaMethodTests(TestCase):
+
+    def test_redirijido_al_home(self):
+        pass
