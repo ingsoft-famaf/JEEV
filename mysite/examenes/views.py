7bd3e94e3b21d3f07f262c2a3c585c0b627a1338
EliQuiroga <sherutiny@gmail.com>
2016-11-23 22:48:23 -0300

    Algoritmo basado en errores finalizado

--- mysite/examenes/views.py
+++ mysite/examenes/views.py
@@ -86,81 +86,115 @@ def examen_encurso(request, materia):
     print type(tema)
  
     for i in range(cant_temas):
-        bla = str(tema[i])
-        temas = TemaE(tema_fk=examenE, nombre_tema=bla)
-        temas.save()
-        print ("print type i%s" %type(i))
-        print ("print i %s" %i)
-    
-    print ("print temas de examen %s "% TemaE.objects.filter(tema_fk=examenE))
-    print type(TemaE.nombre_tema)
+        nombreTema = str(tema[i])
+        temas = guardar_Tema(examenE, nombreTema)
+    temaActual = 0
     return render(request, 'examenes/encurso.html' ,
-                    {'examenE':examenE})
+                    {'examenE':examenE, 'temaActual':temaActual})
 
 
-def respPregErrores(request, examenE_id):
+def respPregErrores(request, examenE_id, temaActual):
     """
     Input: HttpRequest y id del examen
     Output: redirige a un html pasándole dos query
     Esta función muestra una pregunta con sus respuestas para que el usuario
     haga la elección de un de ellas.
     """
+    print("print de temaActual %s" %temaActual)
+    print type(temaActual)
+    temaActual = int(temaActual)
+    print type(temaActual)
+    print("print de temaActual 2 %s" %temaActual)
     examen = get_object_or_404(ExamErrores, pk=examenE_id)
-    
-    query = examen.nombre_materia
-    print ("respPregErrores primera query: %s" % query)
-    tema1 = examen.nombre_tema
-    test = []
-    test = [x.encode('UTF8') for x in tema1]
-    print "prueba test"
-    print type(test)
-    print test
-    query_list = []
-    nueva_lista = []
-    
-    print ("tema1 en el examen: %s"% tema1)
- #   count_temas = 0
+    materia = examen.nombre_materia
+    temas = TemaE.objects.filter(tema_fk=examen)
+    #nTema = temas[0]
+   # print ("print ntema %s" %nTema)
     randomm =[]
-    count_temas = len(tema1)
-  #  count_temas = 2
-    list_preguntas_count=[]
-    print ("count_temas es %s" % count_temas)
-
-    for i in range(count_temas):
-        queryQ = Question.objects.filter(nombre_tema=str(tema1[i])).filter(nombre_materia=query)
-        query_list.insert(i,queryQ)
-        print ("print queryQ: %s"% queryQ)
-    print ("print query_list[0]: %s"% query_list[0])
-
-    largo2 = 0
-    for k in range(count_temas):   
-        largo1= query_list[k].count()
-        print largo1
-        largo2 += largo1
-    print ("el largo2 es %s" % largo2)
-    #si pide mas preguntas de las que tenemos disminuimos la cantidad
-    #para no generar conflictos de bordes
-    print ("print examen.cantidad_preg: %s"% examen.cantidad_preg)
-    if examen.cantidad_preg > largo2:
-        examen.cantidad_preg = largo2
-
-    print examen.cantidad_preg
-    print ("la pregunta_actual es %s" % examen.pregunta_actual) 
+    cantidad_temas = temas.count()
     if examen.pregunta_actual == examen.cantidad_preg:
-        return render(request, 'examenes/finalizo.html',
-                      {'examenE': examen})
-    print examen.cantidad_preg
-    #query con las respuestas ya respondidas
-    queryresp = PregRespE.objects.filter(examen = examen)
-    print ("queryresp %s" % queryresp)
-    #se filtran las ya respondidas
-    querys = filter_query(largo2,queryresp)
-    randomm = random.sample(querys, 1)
+        nota = examen.preguntas_correctas
+        nota1 = examen.cantidad_preg
+        examen.porcentaje = ((nota * 100) / nota1)
+        examen.save()
+        return render(request, 'examenes/finalizo.html', {'examen': examen})
+    if cantidad_temas == 1:
+        nTema = temas[0]
+        preguntas = Question.objects.filter(nombre_tema=nTema).filter(nombre_materia=nombreMateria).filter(reportada=False)
+    else:
+        preguntas= Question.objects.filter(nombre_tema=temas[temaActual]).filter(nombre_materia=materia).filter(reportada=False)
+    queryresp = PregRespE.objects.filter(examen=examen)
+    query = filter_query(preguntas, queryresp)
+    cant_query = query.count()
+    print ("query %s" %query)
+    if cant_query == 0:
+        if temaActual == (cantidad_temas - 1):
+            temaActual = 0
+            for i in range(cantidad_temas):
+                preguntas= Question.objects.filter(nombre_tema=temas[temaActual]).filter(nombre_materia=materia).filter(reportada=False)
+                query = filter_query(preguntas, queryresp)
+                query_count= query.count()
+                if query_count == 0:
+                    temaActual +=1 
+                else:
+                    break
+        else:
+            temaActual+= 1
+        print ("tema actual %s" %temaActual)
+        preguntas= Question.objects.filter(nombre_tema=temas[temaActual]).filter(nombre_materia=materia).filter(reportada=False)
+        query = filter_query(preguntas, queryresp)
+    randomm = random.sample(query, 1)
     pregunta = randomm[0]
-    PregRespE.objects.create(examenE = examen, question = pregunta)
+    print ("la pregunta es: %s" %pregunta)
+    PregRespE.objects.create(examen=examen, question=pregunta)
+    return render(request, 'examenes/respPregErrores.html',
+                  {'pregunta': pregunta, 'examenE': examen, 'temaActual':temaActual})
 
-    return render(request,'examenes/respPregErrores.html',
-                  {'pregunta': pregunta,'examenE':examen})
+
+def respuestaE(request, examenE_id, temaActual):
+    """
+    Input: HttpRequest y id del examen
+    Output: redirige a un html pasándole una query
+    Esta función recoge la respuesta seleccionada y le indica al usuario si
+    es correcta o no. Si no responde en el tiempo predeterminado le indica que
+    la respuesta es incorrecta.
+    """
+
+    print("type temaActual %s" % type(temaActual))
+    if request.method == 'POST':
+        respuesta_id = request.POST['respuesta']
+        examen = get_object_or_404(ExamErrores, pk=examenE_id)
+        examen.pregunta_actual +=1
+        examen.save()
+        cantidad_temas= TemaE.objects.filter(tema_fk=examen).count()
+        respuesta = get_object_or_404(Answer, pk=respuesta_id)
+        cant_correctas=examen.preguntas_correctas
+        if respuesta.es_correcta:
+            examen.preguntas_correctas +=1
+            temaActual = int(temaActual)
+            temaActual +=1
+            print("type temaActual  2%s" % type(temaActual))
+        else :
+            randomm = []
+            examen.preguntas_incorrectas +=1
+        examen.save()
+
+        print ("examen.preguntas_incorrectas  %s" %examen.preguntas_incorrectas )
+        print("temaActual %s" %temaActual)
+        print("cantidad_temas - 1 %s" %(cantidad_temas - 1))
+        if temaActual > (cantidad_temas - 1) and cant_correctas < examen.preguntas_correctas :
+            temaActual = 0
+
+           # if temaActual > cantidad_temas -1:
+        return render(request, 'examenes/respuestaE.html',
+                      {'respuesta':respuesta, 'examenE':examen, 'temaActual':temaActual})
+    examen = get_object_or_404(ExamErrores, pk=examenE_id)
+    examen.pregunta_actual +=1
+    examen.preguntas_incorrectas +=1
+    examen.save()
+    print("print de temaActual en respuestaE%s" %temaActual)
+    return render(request, 'examenes/respuestaE.html', {'examenE':examen, 'temaActual':temaActual})
+
 
 
 """ Algoritmo aleatorio"""
